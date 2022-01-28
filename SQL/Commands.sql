ee12f7534925dd4e13431bd6e323d1ba7141ebfc
JosephFash <97948399+JosephFash@users.noreply.github.com>
2022-01-28 15:16:04 +0000

    SQL Zoo Queries

---
+++ SQL/Commands.sql
@@ -0,0 +1,13 @@
+UPDATE animals
+SET Origin = Asia
+WHERE Animal_Name = Lion;
+
+DELETE FROM animals
+WHERE Breed = Marsupial;
+
+UPDATE Cage
+SET Date_of_last_cleaning = 2020-11-10
+WHERE Animal = Kangaroo
+
+DELETE FROM Cage
+WHERE Size = S
