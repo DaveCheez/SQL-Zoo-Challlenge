ef9f138cf901417a5a80b956aa8cc9036f012280
LukeWebster89 <webstarsince89@hotmail.co.uk>
2022-01-28 15:40:38 +0000

    LW:commit changes to schema file

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,10 +1,30 @@
-create database zoo_db;
+CREATE database group2zoo;
 
-use zoo_db;
+USE group2zoo;
 
-create table Animals (
-    name varchar(60),
-    weight int,
-    breed varchar(60),
-    origin varchar(60)
+CREATE TABLE animals (
+animal_id INT AUTO_INCREMENT,
+PRIMARY KEY (animal_id),
+animal_name VARCHAR(30),
+weight TINYINT,
+breed VARCHAR(50),
+origin VARCHAR(50)
 );
+ 
+ CREATE TABLE cage (
+ cage_id INT AUTO_INCREMENT,
+ PRIMARY KEY (cage_id),
+ animal VARCHAR(30),
+ size CHAR(1),
+zone_location CHAR(1),
+date_of_last_cleaning DATE
+);
+
+ CREATE TABLE zoo_properties (
+ zoo_id INT AUTO_INCREMENT,
+ PRIMARY KEY (zoo_id),
+ zones VARCHAR(20),
+ entry_price VARCHAR(30),
+ animals_in_zone VARCHAR(30),
+ staff_in_zone VARCHAR(30)
+ );
