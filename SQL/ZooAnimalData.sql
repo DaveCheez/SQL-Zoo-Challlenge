234bb9ea29da235c225ac6dd17179988fd00d265
callumcound <callumcound@protonmail.com>
2022-01-28 14:38:42 +0000

    CC: First animal commit

---
+++ SQL/ZooAnimalData.sql
@@ -0,0 +1,2 @@
+INSERT INTO Animals (Animal_Name, Weight, Breed, Origin) VALUES (Pangolin, 40, Mammal, Africa), (Lion, 190, Mammal, Africa)
+, (Eel, 10, Fish, Europe), (Kangaroo, 90, Marsupial, Oceania);
