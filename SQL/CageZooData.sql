5edcbe6725bc31276c81a3a3e0132025ba8d506c
JosephFash <97948399+JosephFash@users.noreply.github.com>
2022-01-28 15:12:11 +0000

    Update CageZooData.sql

--- SQL/CageZooData.sql
+++ SQL/CageZooData.sql
@@ -1,2 +1,2 @@
-INSERT INTO Cage (Animal, Size, Location, Date_of_last_cleaning) VALUES (Pangolin, S, B, 1965-06-14), (Lion, L, A, 20201-01-25),
+INSERT INTO Cage (Animal, Size, Location, Date_of_last_cleaning) VALUES (Pangolin, S, B, 1965-06-14), (Lion, L, A, 2020-01-25),
 (Eel, S, C, 2020-12-31), (Kangaroo, M, B, 1995-03-25); 
